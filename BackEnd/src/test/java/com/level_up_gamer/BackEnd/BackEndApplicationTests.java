3dcdefaf3d3e18c604721056f7b5a4f49d41ca95
COdelValle <c.ormeno@duocuc.cl>
2025-11-14 11:03:46 -0300

    Initialize Spring Boot backend project
    Add initial Spring Boot backend structure for LEVEL UP GAMER, including Maven wrapper, configuration files, main application class, and a basic test. Project is set up with dependencies for Spring Data JPA, Security, Web, Oracle JDBC, Lombok, and testing.

---
+++ BackEnd/src/test/java/com/level_up_gamer/BackEnd/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.level_up_gamer.BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
