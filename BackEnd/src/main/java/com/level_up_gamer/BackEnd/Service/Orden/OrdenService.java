3bcd350c94ed92ec44627a33e18b8fd5d80fe39a
COdelValle <c.ormeno@duocuc.cl>
2025-11-21 12:48:10 -0300

    Add OrdenService for order management
    Introduces OrdenService with methods to retrieve, save, and delete orders by ID, number, or object. This service provides a central point for order-related business logic in the application.

---
+++ BackEnd/src/main/java/com/level_up_gamer/BackEnd/Service/Orden/OrdenService.java
@@ -0,0 +1,55 @@
+package com.level_up_gamer.BackEnd.Service.Orden;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.level_up_gamer.BackEnd.Model.Orden.Orden;
+import com.level_up_gamer.BackEnd.Repository.Orden.OrdenRepository;
+
+@Service
+public class OrdenService {
+    @Autowired
+    private OrdenRepository repository;
+
+    // Obtener info:
+        // Obtener todas las ordenes:
+        public List<Orden> getOrdenes(){
+            return repository.findAll();
+        }
+
+        // Obtener una orden por el ID:
+        public Orden getOrdenByID(Long id){
+            return repository.findById(id).orElse(null);
+        }
+
+        // Obtener una orden por el número(String):
+        public Orden getOrdenByNumero(String numero){
+            return repository.findByNumero(numero).orElse(null);
+        }
+    
+    // Guardar(agregar o actualizar orden) orden:
+    public Orden saveOrden(Orden o){
+        return repository.save(o);
+    }
+
+    // Eliminar orden:
+        // Por Objeto:
+        public Boolean deleteOrden(Orden o){
+            repository.delete(o);
+            return !repository.existsById(o.getId());
+        }
+
+        // Por ID:
+        public Boolean deleteOrdenById(Long id){
+            repository.deleteById(id);
+            return !repository.existsById(id);
+        }
+
+        // Por Número(String):
+        public Boolean deleteOrdenByNumero(String numero){
+            repository.deleteByNumero(numero);
+            return !repository.existsByNumero(numero);
+        }
+}
